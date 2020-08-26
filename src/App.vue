98eb7f74eefedec3010957cedf89a85607f0706a
Marc <g40419@gmail.com>
2020-08-26 14:43:22 +0800

    建立登入註冊頁面

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
-  <div id="restaurant-forum">   
-    <router-view/>
+  <div id="restaurant-forum"> 
+    <Navbar />
+    <main class="mt-5 bg-white" role="main">
+      <router-view/>
+    </main>
   </div>
 </template>
 
 <script>
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar'
 export default {
-  name: 'App'
+  components: {
+    Navbar
+  }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import NotFound from '../views/NotFound'
+import SignIn from '../views/SignIn'
+import SignUp from '../views/SignUp'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
+    path: '/signin',
+    name: 'sign-in',
+    component: SignIn
+  },
+  {
+    path: '/signup',
+    name: 'sign-up',
+    component: SignUp
+  },
+  {
     path: '*',
-    name: 'NotFound',
+    name: 'not-found',
     component: NotFound
   }
 ]
