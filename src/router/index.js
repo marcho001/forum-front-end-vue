71b02fe283ce768eef32ca062cd2debaca727ff4
Marc <g40419@gmail.com>
2020-08-26 14:55:49 +0800

    建立各頁面的路由和頁面

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import NotFound from '../views/NotFound'
 import SignIn from '../views/SignIn'
-import SignUp from '../views/SignUp'
-
+import Restaurants from '../views/Restaurants'
 Vue.use(VueRouter)
 
   const routes = [
+  {
+    path: '/',
+    name: 'root',
+    redirect: '/restaurants'
+  },
   {
     path: '/signin',
     name: 'sign-in',
@@ -15,7 +19,27 @@ Vue.use(VueRouter)
   {
     path: '/signup',
     name: 'sign-up',
-    component: SignUp
+    component: () => import('../views/SignUp')
+  },
+  {
+    path: '/restaurants',
+    name: 'restaurants',
+    component: Restaurants
+  },
+  {
+    path: '/restaurants/feeds',
+    name: 'restaurants-feeds',
+    component: () => import('../views/RestaurantsFeeds')
+  },
+  {
+    path: '/restaurants/top',
+    name: 'restaurants-tops',
+    component: () => import('../views/RestaurantsFeeds')
+  },
+  {
+    path: '/users/top',
+    name: 'users-top',
+    component: () => import('../views/UsersTop')
   },
   {
     path: '*',
